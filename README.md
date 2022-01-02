9a3ca893ea4a0f59ca2552183f5fa2d6b38c12b9
Allan Napier <49874689+allannapier@users.noreply.github.com>
2022-01-02 16:26:48 +0000

    added some error checking

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cloudsearch
+
+This script will allow you to quickly search both your local directories and a S3 Bucket for a file using a search term.
+
+Please ensure you fill in the s3_bucket and local_path, if you don't wish to search one of them just set the variable to '' and ensure it has been omitted from the locations list underneath.
--- search.py
+++ search.py
@@ -4,52 +4,60 @@ import datetime
 import boto3
 from botocore.exceptions import ClientError
 import pprint
+import argparse
 
 #PLEASE FILL IN THE TWO VARIABLES BELOW WITH CORRECT DETAILS BEFORE RUNNING
 #YOU MUST HAVE YOUR AWS CREDENTIALS SET IN ENVIRONMENT ALREADY, DON'T SAVE THEM IN THE CODE, THAT IS NOT SECURE
-s3_bucket = ''
-local_path = '/users/xxx/Documents/'
+s3_bucket = None
+local_path = None
 
 def index_files(locations,my_path=None):
     filelist = []
-    if not my_path:
-        my_path = '/users/allannapier/Documents/GitHub'
+    if not s3_bucket or local_path:
+        print('You have not set the variables yet for s3_bucket and local_path, please do this before running again.')
+        quit()
+    else:
+        if 'local' in locations:
 
-    if 'local' in locations:
+            for file in glob.iglob(my_path + '/**/*', recursive=True):
 
-        for file in glob.iglob(my_path + '/**/*', recursive=True):
+                stat_info = os.stat(file)
+                # get size of file in bytes
+                if os.path.isfile(file):
+                    size = round(stat_info.st_size/(1024*1024),2)
+                    t = os.path.getmtime(file)
+                    fileinfo = {"filename":file,"sizeMB":size,"lastmodified":datetime.datetime.fromtimestamp(t),"provider":"local"}
+                    filelist.append(fileinfo)
 
-            stat_info = os.stat(file)
-            # get size of file in bytes
-            if os.path.isfile(file):
-                size = round(stat_info.st_size/(1024*1024),2)
-                t = os.path.getmtime(file)
-                fileinfo = {"filename":file,"sizeMB":size,"lastmodified":datetime.datetime.fromtimestamp(t),"provider":"local"}
-                filelist.append(fileinfo)
+        if 'S3' in locations:
+            s3 = boto3.client('s3')
+            for key in s3.list_objects(Bucket=s3_bucket)['Contents']:
+                    fileinfo = {"filename":key['Key'],"sizeMB":round(key['Size']/(1024*1024),2),"lastmodified":key['LastModified'],"provider":"S3"}
+                    filelist.append(fileinfo)
 
-    if 'S3' in locations:
-        s3 = boto3.client('s3')
-        for key in s3.list_objects(Bucket=s3_bucket)['Contents']:
-                fileinfo = {"filename":key['Key'],"sizeMB":round(key['Size']/(1024*1024),2),"lastmodified":key['LastModified'],"provider":"S3"}
-                filelist.append(fileinfo)
-
-    return filelist
+        return filelist
 
 def search_for_file(filelist,term):
     found = True
     result = []
     while found:
-        search_result = next((x for x in filelist if term in x["filename"]),None )
+        
+        search_result = next((x for x in filelist if term[0] in x["filename"]),None )
         if search_result:
             result.append(search_result)
             filelist.remove(search_result)
         else:
             found = False
+        
     return result
 
+parser = argparse.ArgumentParser(description='Search for files')
+parser.add_argument('searchterm', metavar='N', type=str, nargs='+',
+                    help='a string to search for in filenames')
+
+args = parser.parse_args()
 
 locations = ['local','S3']
 files = index_files(locations,local_path)
 
-searchterm = 'favicon'
-pprint.pprint(search_for_file(files,searchterm))
+pprint.pprint(search_for_file(files,args.searchterm))
